8256bdab49bde47fd64bafe078525c9455df85ec
Parkhanyoung <80797824+Parkhanyoung@users.noreply.github.com>
2021-10-11 16:19:41 +0900

    remove all

--- myproject/scrap/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from scrap.models import Courdiv, College, ElectivesGroup, Department, Course
-
-admin.site.register(Courdiv)
-admin.site.register(College)
-admin.site.register(ElectivesGroup)
-admin.site.register(Department)
-admin.site.register(Course)
--- test.py
+++
@@ -1,3 +0,0 @@
-word = "asd <br> dsa"
-word2 = word.split('<br>')
-print(word2)
